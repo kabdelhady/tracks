1ac6ca8199665b1dca1e6f34193c4174291ec8c0
Khaled Abdelhady <khaled.abdelhady@badrit.com>
2012-12-30 12:53:36 +0200

    remove code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,16 +23,17 @@ class ApplicationController < ActionController::Base
   prepend_before_filter :login_required
   prepend_before_filter :enable_mobile_content_negotiation
   after_filter :set_charset
-  before_filter :restore_db
+  # before_filter :restore_db
 
   # By default, sets the charset to UTF-8 if it isn't already set
   def set_charset
     headers["Content-Type"] ||= "text/html; charset=UTF-8"
   end
 
-  def restore_db
-    RestoreDate.first.last_restore
-  end
+  # def restore_db
+  #   last_restore = RestoreDate.first.last_restore
+  #   if last_restore - 
+  # end
 
   def set_locale
     locale = params[:locale] # specifying a locale in the request takes precedence
